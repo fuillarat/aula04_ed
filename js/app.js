a11d1e622428682c535c70715d23c0edd65a4c99
fuillarat <150846604+fuillarat@users.noreply.github.com>
2025-03-07 12:30:13 -0300

    [feat] atualização do app.js

--- js/app.js
+++ js/app.js
@@ -52,3 +52,56 @@ let frutas_sel = frutas.slice(1,4)
 
 console.log(frutas_sel)
 
+let frutas_c = frutas.splice(1,1,"manga")
+console.log(frutas_sel)
+
+
+situacao = frutas.includes("Caju");
+console.log(situacao)
+
+//unificando elementos a um texto separado por virgulas
+let texto = frutas .join('\n ')
+let texto2 = frutas.join(', ')
+let texto3 = frutas.join(' - ')
+
+console.log(texto)
+console.log(texto2)
+console.log(texto3)
+
+
+let numeros =[0,1,2,3,4,5,6,7,8,9,10]
+
+let tabuada = numeros.map(x=>x*2)
+
+console.log(tabuada)
+
+
+//filter
+
+let pares = numeros.filter(x=>x % 2 ==0 )
+console.log(pares)
+
+let impares = numeros.filter(x=>x % 2 !=0 )
+console.log(impares)
+
+let pares2 = numeros.filter(x => {
+    if(x ==0) {} else {
+        return x % 2 == 0
+    }
+})
+console.log(pares2);
+
+// console.log(pares)
+
+
+let loc = numeros.find(x=> x>5)
+console.log(loc)
+
+loc = numeros.findIndex(x=> x>5)
+console.log(loc)
+
+let return_some = numeros.some(x=> x>5)
+console.log(return_some)
+
+let return_every = numeros.every(x=> x>=0)
+console.log(return_every)
